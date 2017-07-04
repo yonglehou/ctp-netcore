fe4f9d2a592865ef9dccf62394b30d17a8f10b4d
charles <yonglehou@live.cn>
2017-07-04 13:36:44 +0800

    init

---
+++ ctptest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestTrade tt = new TestTrade();
+			tt.Run();
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+			tt.ShowInfo();
+			Console.ReadKey(true);
+
+			TestQuote tq = new TestQuote();
+			tq.Run();
+			Console.WriteLine("Press any key to continue . . . ");
+			Console.ReadKey(true);
+
+			tt.Release();
+			tq.Release();
+			Console.ReadKey(true);
+		}
+	}
+}
